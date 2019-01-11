2fe2881200236084221b878e8d0d5d727e7ccfc6
YouruiGuo <yourui@ualberta.ca>
2019-01-11 12:55:04 -0700

    try

--- assets/js/testmix.js
+++ assets/js/testmix.js
@@ -1,37 +1,10 @@
 var AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
 var audio = new AudioContext();
 
-function test() {
-    // Stereo
-  var channels = 2;
-  // Create an empty two second stereo buffer at the
-  // sample rate of the AudioContext
-  var frameCount = audio.sampleRate * 2.0;
-  var myArrayBuffer = audio.createBuffer(channels, frameCount, audio.sampleRate);
-  for (var channel = 0; channel < channels; channel++) {
-     // This gives us the actual array that contains the data
-     var nowBuffering = myArrayBuffer.getChannelData(channel);
-     for (var i = 0; i < frameCount; i++) {
-       // Math.random() is in [0; 1.0]
-       // audio needs to be in [-1.0; 1.0]
-       nowBuffering[i] = Math.random() * 2 - 1;
-     }
-   }
-   // Get an AudioBufferSourceNode.
-   // This is the AudioNode to use when we want to play an AudioBuffer
-   var source = audio.createBufferSource();
-   // set the buffer in the AudioBufferSourceNode
-   source.buffer = myArrayBuffer;
-   // connect the AudioBufferSourceNode to the
-   // destination so we can hear the sound
-   source.connect(audio.destination);
-   // start the source playing
-   source.start();
-}
-
 function stopAudio() {
   audio.close().then(function() {audio=new AudioContext();});
 }
+
 async function decodeAudioDataAsync(data) {
    return new Promise((resolve, reject) => {
      audio.decodeAudioData(data, b => {
@@ -42,37 +15,21 @@ async function decodeAudioDataAsync(data) {
    })
  }
 
-async function loadFiles(fP) {
-  let filePaths = [];
-  await fP.then(function (value) { filePaths = value;});
-  let buffers = [];
-  for (let f of filePaths) {
-    let response = await fetch(f);
-    let arrayBuffer = await response.arrayBuffer();
-    let audioBuffer = await decodeAudioDataAsync(arrayBuffer);
-    buffers.push(audioBuffer);
-  }
-  return buffers;
+async function loadFile(filepath) {
+
+  let response = await fetch(filepath);
+  let arrayBuffer = await response.arrayBuffer();
+  let audioBuffer = await decodeAudioDataAsync(arrayBuffer);
+
+  return audioBuffer;
 }
 
-function playTracks(buffers) {
-  var channel = 2;
-  var frameCount = audio.sampleRate*_maxDuration(buffers);
-  let output = audio.createBuffer(channel, frameCount, audio.sampleRate);
-
-  for(let buffer of buffers) {
-    for (var c = 0; c < channel; c++) {
-      nowBuffering = output.getChannelData(c);
-      for(var i = 0; i < frameCount; i++){
-         nowBuffering[i] += buffer.getChannelData(c)[i];
-      }
-    }
-  }
+function playTrack(buffer) {
   // Get an AudioBufferSourceNode.
   // This is the AudioNode to use when we want to play an AudioBuffer
   var source = audio.createBufferSource();
   // set the buffer in the AudioBufferSourceNode
-  source.buffer = output;
+  source.buffer = buffer;
   source.loop = true;
   // connect the AudioBufferSourceNode to the
   // destination so we can hear the sound
@@ -82,15 +39,19 @@ function playTracks(buffers) {
 }
 
 function handleFilesSelect(fP) {
-
-  loadFiles(fP).then((track) => {
-    // check if context is in suspended state (autoplay policy)
-    if (audio.state === 'suspended') {
-      audio.resume();
-    }
-    playTracks(track);
+  let filePaths = [];
+  await fP.then(function (value) { filePaths = value;});
+  filePaths.forEach(function (f) {
+    loadFile(f).then((track) => {
+      // check if context is in suspended state (autoplay policy)
+      if (audio.state === 'suspended') {
+        audio.resume();
+      }
+      playTrack(track);
+    })
   })
-    
+
+
 }
 
 function _maxDuration(buffers) {
