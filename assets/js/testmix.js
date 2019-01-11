a8a5e1e273485fbfeffe8b7e5a3e8617ebd1acb2
YouruiGuo <yourui@ualberta.ca>
2019-01-11 13:23:18 -0700

    ahh

--- assets/js/testmix.js
+++ assets/js/testmix.js
@@ -1,33 +1,6 @@
 var AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
 var audio = new AudioContext();
 
-function test() {
-    // Stereo
-  var channels = 2;
-  // Create an empty two second stereo buffer at the
-  // sample rate of the AudioContext
-  var frameCount = audio.sampleRate * 2.0;
-  var myArrayBuffer = audio.createBuffer(channels, frameCount, audio.sampleRate);
-  for (var channel = 0; channel < channels; channel++) {
-     // This gives us the actual array that contains the data
-     var nowBuffering = myArrayBuffer.getChannelData(channel);
-     for (var i = 0; i < frameCount; i++) {
-       // Math.random() is in [0; 1.0]
-       // audio needs to be in [-1.0; 1.0]
-       nowBuffering[i] = Math.random() * 2 - 1;
-     }
-   }
-   // Get an AudioBufferSourceNode.
-   // This is the AudioNode to use when we want to play an AudioBuffer
-   var source = audio.createBufferSource();
-   // set the buffer in the AudioBufferSourceNode
-   source.buffer = myArrayBuffer;
-   // connect the AudioBufferSourceNode to the
-   // destination so we can hear the sound
-   source.connect(audio.destination);
-   // start the source playing
-   source.start();
-}
 
 function stopAudio() {
   audio.close().then(function() {audio=new AudioContext();});
@@ -88,9 +61,13 @@ function handleFilesSelect(fP) {
     if (audio.state === 'suspended') {
       audio.resume();
     }
-    playTracks(track);
+    var controller = document.getElementById("clickdiv");
+    controller.addEventListener('click', function() {
+      playTracks(track);
+    })
+
   })
-    
+
 }
 
 function _maxDuration(buffers) {
