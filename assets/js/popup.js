b28b643a5765cdf706a5d1c3067aba3728077453
YouruiGuo <yourui@ualberta.ca>
2019-01-22 10:40:41 -0700

    ui change

---
+++ assets/js/popup.js
@@ -0,0 +1,12 @@
+var app = new Framework7();
+
+var $$ = Dom7;
+
+// DOM events for About popup
+$$('.popup-about').on('popup:open', function (e, popup) {
+  console.log('About popup open');
+});
+$$('.popup-about').on('popup:opened', function (e, popup) {
+  console.log('About popup opened');
+});
+
