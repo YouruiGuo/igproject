d55cc064d4a7df07bc995b703d0a2a9d74b52df2
YouruiGuo <yourui@ualberta.ca>
2019-01-27 23:19:12 -0700

    ui update

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -10,3 +10,21 @@ $$('.popup-about').on('popup:opened', function (e, popup) {
   console.log('About popup opened');
 });
 
+class musicPlayer {
+	constructor() {
+		this.play = this.play.bind(this);
+		this.playBtn = document.getElementById('play');
+		this.playBtn.addEventListener('click', this.play);
+		this.controlPanel = document.getElementById('control-panel');
+	}
+
+	play() {
+		let controlPanelObj = this.controlPanel;
+		Array.from(controlPanelObj.classList).find(function(element){
+					return element !== "active" ?
+controlPanelObj.classList.add('active') : 		controlPanelObj.classList.remove('active');
+			});
+	}
+}
+
+const newMusicplayer = new musicPlayer();
