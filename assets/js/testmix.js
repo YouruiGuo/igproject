a956f40423e48c5b8e824e37b00e22a8303c3e73
YouruiGuo <yourui@ualberta.ca>
2019-02-27 15:24:36 -0700

    mute implemented

--- assets/js/testmix.js
+++ assets/js/testmix.js
@@ -1,5 +1,8 @@
 var AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
 var audio = new AudioContext();
+var buffers = {};
+var allsources = {};
+var pauses = {}; // 0: playing, else: pause at some time
 
 function stopAudio() {
   audio.close().then(function () {audio = new AudioContext();});
@@ -23,7 +26,8 @@ async function loadFiles(fP) {
     let response = await fetch(f);
     let arrayBuffer = await response.arrayBuffer();
     let audioBuffer = await decodeAudioDataAsync(arrayBuffer);
-    buffers.push(audioBuffer);
+    //buffers.push(audioBuffer);
+    buffers[f] = audioBuffer;
   }
   return buffers;
 }
@@ -31,27 +35,52 @@ async function loadFiles(fP) {
 function playTracks(buffers) {
   var channel = 2;
   var frameCount = audio.sampleRate*_maxDuration(buffers);
-  let output = audio.createBuffer(channel, frameCount, audio.sampleRate);
 
-  for(let buffer of buffers) {
+  for(var key in buffers) {
+    var buffer = buffers[key]
+    let output = audio.createBuffer(channel, frameCount, audio.sampleRate);
     for (var c = 0; c < channel; c++) {
       nowBuffering = output.getChannelData(c);
       for(var i = 0; i < frameCount; i++){
          nowBuffering[i] += buffer.getChannelData(c)[i];
       }
     }
+    // Get an AudioBufferSourceNode.
+    // This is the AudioNode to use when we want to play an AudioBuffer
+    var source = audio.createBufferSource();
+    // set the buffer in the AudioBufferSourceNode
+    source.buffer = output;
+    source.loop = true;
+    // connect the AudioBufferSourceNode to the
+    // destination so we can hear the sound
+    source.connect(audio.destination);
+    allsources[key] = source;
+    var playInfo = {};
+    playInfo["startedAt"] = -1;
+    playInfo["paused"] = 0; // playing
+    playInfo["pausedAt"] = -1;
+    playInfo["interval"] = 0;
+    pauses[key] = playInfo;
+    // start the source playing
+    source.start();
   }
-  // Get an AudioBufferSourceNode.
-  // This is the AudioNode to use when we want to play an AudioBuffer
-  var source = audio.createBufferSource();
-  // set the buffer in the AudioBufferSourceNode
-  source.buffer = output;
-  source.loop = true;
-  // connect the AudioBufferSourceNode to the
-  // destination so we can hear the sound
-  source.connect(audio.destination);
-  // start the source playing
-  source.start();
+}
+
+function playAndPauseSingleTrack(filepath) {
+  let source = allsources[filepath];
+  let playInfo = pauses[filepath];
+  if (playInfo["paused"]) {
+    playInfo["paused"] = 0; // playing
+    playInfo["interval"] += playInfo["pausedAt"] - playInfo["startedAt"];
+    playInfo["interval"] %= _maxDuration(buffers);
+    source.play(0, temp/1000);
+    playInfo["startedAt"] = Date.now();
+  }
+  else{
+    playInfo["paused"] = 1; // paused
+    source.stop(0);
+    playInfo["pausedAt"] = Date.now();
+  }
 }
 
 function playAndPause() {
