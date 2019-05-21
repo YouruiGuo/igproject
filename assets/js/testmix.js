2772a19c00870f6f37271729ced1b179dd30d126
YouruiGuo <yourui@ualberta.ca>
2019-05-21 12:54:24 -0600

    distance listening

--- assets/js/testmix.js
+++ assets/js/testmix.js
@@ -1,5 +1,13 @@
 var AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
 var audio = new AudioContext();
+var panner = audio.createPanner();
+panner.panningModel = 'HRTF';
+panner.distanceModel = 'inverse';
+panner.refDistance = 1;
+panner.maxDistance = 100;
+panner.coneInnerAngle = 360;
+panner.coneOuterAngle = 0;
+panner.coneOuterGain = 0;
 
 var buffers = {};
 var allsources = {};
@@ -131,23 +139,18 @@ function playTracks(buffers, loop) {
     // This is the AudioNode to use when we want to play an AudioBuffer
     var source = audio.createBufferSource();
     var g;
-//    if (!gains[key]) {
-      g = audio.createGain();
-      g.gain.value = 1;
-      gains[key] = g;
-   // }
-/*
-    else{
-      console.log(gains[key]);
-      g = gains[key];
-    }*/
+    g = audio.createGain();
+    g.gain.value = 1;
+    gains[key] = g;
+
     // set the buffer in the AudioBufferSourceNode
     source.buffer = output;
     source.loop = loop;
     mute[key] = 0; // un-muted
     console.log(gains[key]);
     source.connect(gains[key]);
-    gains[key].connect(audio.destination);
+    panner.connect(gains[key]).connect(audio.destination);
+
     // start the source playing
     source.start(0);
     if (audio.state === "running") {
@@ -173,39 +176,7 @@ function playAndPause() {
       audio.suspend();
     }
 }
-/*
-async function firstHandleFilesSelectIntro(fP) {
-  let filePaths = fP;
-//  await fP.then(function (value) { filePaths = value;});
-  loadFilesList(fP).then((track) => {
-  console.log(track);
 
-  var icon = $$('.playintro');
-  icon.on('click', function() {
-     playTracks(track, true);
-     icon.toggleClass('active');
-     return false;
-  });
-    var controller = document.getElementById("introplay");
-    controller.addEventListener('click', function() {
-      playTracks(track);
-    }, {once: true});
-  });
-}
-*/
-/*
-async function firstHandleFilesSelect(fP) {
-  let filePaths = [];
-  await fP.then(function (value) { filePaths = value;});
-  loadFiles(fP).then((track) => {
-
-   var controller = document.getElementById("play-btn");
-    controller.addEventListener('click', function() {
-      playTracks(track, true);
-    }, {once: true});
-  });
-}
-*/
 async function handleFilesSelect(fP) {
   let filePaths = [];
   await fP.then(function (value) { filePaths = value;});
@@ -218,24 +189,3 @@ async function handleFilesSelect(fP) {
 function _maxDuration(buffers) {
  return Math.max.apply(Math, buffers.map(buffer => buffer.duration));
 }
-/*
-function playSingleTrack(buffer, fp){
-  var source = audio.createBufferSource();
-  var g = audio.createGain();
-  source.buffer = buffer;
-  source.loop = true;
-  mute[fp] = 0;
-  gains[fp] = g;
-  source.connect(g);
-  g.connect(audio.destination);
-  // start the source playing
-  source.start();
-}
-async function loadSingleFile(fp) {
-  var filepath=fp;
-  let response = await fetch(filepath);
-  let arrayBuffer = await response.arrayBuffer();
-  let audioBuffer = await decodeAudioDataAsync(arrayBuffer);
-  buffers[fp] = audioBuffer;
-  return audioBuffer;
-}*/
